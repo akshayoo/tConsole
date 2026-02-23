3723819a6c8960e651e2988d7a334444abc25e4f
AK <akshayramesh543@gmail.com>
2026-02-23 23:04:22 +0530

    ege

--- binfk_console/src/app/layout.js
+++ binfk_console/src/app/layout.js
@@ -1,6 +1,14 @@
 
 import "./globals.css"
 
+export const metadata = {
+  title: "tCONSOLE",          
+  description: "Your app description here",
+  icons: {
+    icon: "/favicon.ico",      
+  },
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
